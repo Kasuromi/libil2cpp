4f3c7d20d120d2c84fe916b854ff6f0c53de148e
Kasuromi <29818914+Kasuromi@users.noreply.github.com>
2021-12-14 22:10:05 +0200

    2018.2.7f1

--- vm/ScopedThreadAttacher.h
+++ vm/ScopedThreadAttacher.h
@@ -10,31 +10,11 @@ namespace vm
     class ScopedThreadAttacher
     {
     public:
-        ScopedThreadAttacher()
-#if !IL2CPP_HAS_NATIVE_THREAD_CLEANUP
-            : m_AttachedThread(NULL)
-#endif
-        {
-#if !IL2CPP_HAS_NATIVE_THREAD_CLEANUP
-            if (il2cpp::vm::Thread::Current() == NULL)
-                m_AttachedThread = il2cpp::vm::Thread::Attach(il2cpp::vm::Domain::GetRoot());
-#else
-            il2cpp::vm::Thread::Attach(il2cpp::vm::Domain::GetRoot());
-#endif
-        }
-
-        ~ScopedThreadAttacher()
-        {
-#if !IL2CPP_HAS_NATIVE_THREAD_CLEANUP
-            if (m_AttachedThread != NULL)
-                il2cpp::vm::Thread::Detach(m_AttachedThread);
-#endif
-        }
+        ScopedThreadAttacher();
+        ~ScopedThreadAttacher();
 
     private:
-#if !IL2CPP_HAS_NATIVE_THREAD_CLEANUP
-        Il2CppThread * m_AttachedThread;
-#endif
+        Il2CppThread* m_AttachedThread;
     };
 }
 }
