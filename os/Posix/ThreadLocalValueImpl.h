c67a2120f92f43ae3313b3dd176c724f4ae9c47b
Kasuromi <29818914+Kasuromi@users.noreply.github.com>
2021-12-15 12:56:29 +0200

    2020.3.0f1

--- os/Posix/ThreadLocalValueImpl.h
+++ os/Posix/ThreadLocalValueImpl.h
@@ -7,7 +7,6 @@
 
 #include <pthread.h>
 
-#define _unused(x) ((void)x)
 namespace il2cpp
 {
 namespace os
@@ -19,15 +18,17 @@ namespace os
         {
             pthread_key_t key;
             int result = pthread_key_create(&key, NULL);
-            IL2CPP_ASSERT(!result);
-            _unused(result);
+            IL2CPP_ASSERT(result == 0);
+            NO_UNUSED_WARNING(result);
 
             m_Key = key;
         }
 
         inline ~ThreadLocalValueImpl()
         {
-            IL2CPP_ASSERT(!pthread_key_delete(m_Key));
+            int result = pthread_key_delete(m_Key);
+            IL2CPP_ASSERT(result == 0);
+            NO_UNUSED_WARNING(result);
         }
 
         inline ErrorCode SetValue(void* value)
